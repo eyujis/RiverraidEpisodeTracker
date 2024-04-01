ba1958ae7f2743748b66dbf4372745acca28f7b0
eyujis <eyuji@gmail.com>
2024-04-01 15:15:41 -0300

    Adjust parameters

--- src/main/java/org/example/mind/codelets/co_episode_cat_learner/COEpisodeCategoryLearner.java
+++ src/main/java/org/example/mind/codelets/co_episode_cat_learner/COEpisodeCategoryLearner.java
@@ -8,12 +8,10 @@ import java.util.stream.Collectors;
 
 public class COEpisodeCategoryLearner {
     double RELEVANCE_THRESHOLD = 3;
-//    double INIT_RELEVANCE = 1;
     double INIT_RELEVANCE = 5;
     double INCREMENT_FACTOR = 2;
-    double DECREMENT_FACTOR = 1;
+    double DECREMENT_FACTOR = 0;
     double MINIMUM_RELEVANCE = 0;
-    double MIN_RECT_DISTANCE = 2;
 
     COEpisodeCategoryFactory cOEpisodeCategoryFactory;
 
